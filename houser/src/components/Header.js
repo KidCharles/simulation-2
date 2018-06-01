18fa558e4072da01decd9fe7ef276d877f8de798
Charles Hatch <charlesghatch@gmail.com>
2018-06-01 15:13:20 -0600

    i got the log !!!!!

--- houser/src/components/Header.js
+++ houser/src/components/Header.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import './Header.css'
+import './Header.css';
+import logo from './logo.png';
 
 export default function Header() {
     return (
-        <div className="Header">
-            {/* <img src='' /> */}
+        <div className="Header" >
+            <img src={logo} />
             <h1>Houser</h1>
         </div>
     )
